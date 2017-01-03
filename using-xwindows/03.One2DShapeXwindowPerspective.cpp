1968036499559e2f7fa8d2db84ed9c90dfe66813
jeevan-gaikwad <jcgaikwad@gmail.com>
2017-01-03 20:52:42 +0530

    Added new opelgl excercises

---
+++ using-xwindows/03.One2DShapeXwindowPerspective.cpp
@@ -0,0 +1,345 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h> 
+#include <memory.h> 
+#include <math.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h> 
+#include <X11/XKBlib.h>
+#include <X11/keysym.h> 
+
+#include <GL/gl.h>
+#include <GL/glx.h>
+//#include <GL/glu.h>
+//namespaces
+using namespace std;
+
+//global variable declarations
+bool bFullscreen=false;
+Display *gpDisplay=NULL;
+XVisualInfo *gpXVisualInfo=NULL;
+Colormap gColormap;
+Window gWindow;
+int giWindowWidth=800;
+int giWindowHeight=600;
+
+GLfloat angleTri=0.0f;
+GLfloat angleSquare=0.0f;
+
+GLXContext gGLXContext;
+
+void perspectiveGL(GLdouble fovY, GLdouble aspect, GLdouble zNear, GLdouble zFar);
+//entry-point function
+int main(void)
+{
+	//function prototypes
+	void CreateWindow(void);
+	void ToggleFullscreen(void);
+	void initialize(void);
+	void display(void);
+	void resize(int,int);
+	void uninitialize();
+	//void spin(void);
+	
+	//variable declarations
+	int winWidth=giWindowWidth;
+	int winHeight=giWindowHeight;
+	
+	bool bDone=false;
+	
+	//code
+	CreateWindow();
+
+	//initialize
+	initialize();
+	
+	//Message Loop
+	XEvent event;
+	KeySym keysym;
+	
+	while(bDone==false)
+	{
+		while(XPending(gpDisplay))
+		{
+			XNextEvent(gpDisplay,&event);
+			switch(event.type)
+			{
+				case MapNotify:
+					break;
+				case KeyPress:
+					keysym=XkbKeycodeToKeysym(gpDisplay,event.xkey.keycode,0,0);
+				switch(keysym)
+					{
+						case XK_Escape:
+							uninitialize();
+							exit(0);
+						case XK_F:
+						case XK_f:
+							if(bFullscreen==false)
+							{
+								ToggleFullscreen();
+								bFullscreen=true;
+							}
+							else
+							{
+								ToggleFullscreen();
+								bFullscreen=false;
+							}
+							break;
+						default:
+							break;
+					}
+					break;
+				case ButtonPress: 
+					switch(event.xbutton.button)
+					{
+						case 1: 
+						    break;
+						case 2: 
+						    break;
+						case 3: 
+						    break;
+						default:
+						    break;
+			}	
+					break;
+				case MotionNotify: 
+					break;
+				case ConfigureNotify: 
+					winWidth=event.xconfigure.width;
+					winHeight=event.xconfigure.height;
+					resize(winWidth,winHeight);
+					break;
+				case Expose: 
+					break;
+				case DestroyNotify:
+					break;
+				case 33:
+					bDone=true;
+					break;
+				default:
+					break;
+			}
+		
+		}
+
+		display();
+		//spin();
+	}
+	
+	uninitialize();
+	return(0);
+}
+
+void CreateWindow(void)
+{
+	//function prorttypes
+	void uninitialize(void);
+
+	//variable declarations
+	XSetWindowAttributes winAttribs;
+	int defaultScreen;
+	int defaultDepth;
+	int styleMask;
+
+	static int frameBufferAttributes[]=
+	{
+		GLX_RGBA,
+		GLX_RED_SIZE, 1,
+		GLX_GREEN_SIZE, 1,
+		GLX_BLUE_SIZE, 1,
+		GLX_ALPHA_SIZE, 1,
+		None 
+	}; 
+	
+	//code
+	gpDisplay=XOpenDisplay(NULL);
+	if(gpDisplay==NULL)
+	{
+		printf("ERROR : Unable To Open X Display.\nExitting Now...\n");
+		uninitialize();
+		exit(1);
+	}
+	
+	defaultScreen=XDefaultScreen(gpDisplay);
+	
+	gpXVisualInfo=glXChooseVisual(gpDisplay,defaultScreen,frameBufferAttributes);
+		
+	winAttribs.border_pixel=0;
+	winAttribs.background_pixmap=0;
+	winAttribs.colormap=XCreateColormap(gpDisplay, 
+					    RootWindow(gpDisplay, gpXVisualInfo->screen), 
+					    gpXVisualInfo->visual,
+ 					    AllocNone);
+	gColormap=winAttribs.colormap;
+
+	winAttribs.background_pixel=BlackPixel(gpDisplay,defaultScreen);
+
+	winAttribs.event_mask= ExposureMask | VisibilityChangeMask | ButtonPressMask | KeyPressMask | PointerMotionMask |
+			       StructureNotifyMask;
+	
+	styleMask=CWBorderPixel | CWBackPixel | CWEventMask | CWColormap;
+	
+	gWindow=XCreateWindow(gpDisplay,
+			      RootWindow(gpDisplay,gpXVisualInfo->screen),
+			      0,
+			      0,
+			      giWindowWidth,
+			      giWindowHeight,
+			      0,
+			      gpXVisualInfo->depth,
+			      InputOutput,
+			      gpXVisualInfo->visual,
+			      styleMask,
+			      &winAttribs);
+	if(!gWindow)
+	{
+		printf("ERROR : Failed To Create Main Window.\nExitting Now...\n");
+		uninitialize();
+		exit(1);
+	}
+	
+	XStoreName(gpDisplay,gWindow,"One 2D Shape Perspective");
+	
+	Atom windowManagerDelete=XInternAtom(gpDisplay,"WM_DELETE_WINDOW",True);
+	XSetWMProtocols(gpDisplay,gWindow,&windowManagerDelete,1);
+	
+	XMapWindow(gpDisplay,gWindow);
+}
+
+void ToggleFullscreen(void)
+{
+	//variable declarations
+	Atom wm_state;
+	Atom fullscreen;
+	XEvent xev={0};
+	
+	//code
+	wm_state=XInternAtom(gpDisplay,"_NET_WM_STATE",False);
+	memset(&xev,0,sizeof(xev));
+	
+	xev.type=ClientMessage;
+	xev.xclient.window=gWindow;
+	xev.xclient.message_type=wm_state;
+	xev.xclient.format=32;
+	xev.xclient.data.l[0]=bFullscreen ? 0 : 1;
+	
+	fullscreen=XInternAtom(gpDisplay,"_NET_WM_STATE_FULLSCREEN",False);
+	xev.xclient.data.l[1]=fullscreen;
+	
+	XSendEvent(gpDisplay,
+	           RootWindow(gpDisplay,gpXVisualInfo->screen),
+	           False,
+	           StructureNotifyMask,
+	           &xev);
+}
+
+void initialize(void)
+{
+	//function prototype
+	void resize(int, int);
+	
+	//code
+	gGLXContext=glXCreateContext(gpDisplay,gpXVisualInfo,NULL,GL_TRUE);
+	
+	glXMakeCurrent(gpDisplay,gWindow,gGLXContext);
+	
+	glClearColor(0.0f,0.0f,1.0f,0.0f);
+	
+	resize(giWindowWidth,giWindowHeight);
+}
+
+void display(void)
+{
+	//code
+	glClear(GL_COLOR_BUFFER_BIT);
+    
+    glLoadIdentity();
+	glTranslatef(0.0f,0.0f,-3.0f);
+	
+	glBegin(GL_TRIANGLES);
+		glColor3f(1.0f,1.0f,1.0f); 
+
+		glVertex3f(0.0f,1.0f,0.0f); //apex of triangle
+		glVertex3f(-1.0f,-1.0f,0.0f); //left-bottom tip of triangle
+		glVertex3f(1.0f,-1.0f,0.0f); //right-bottom tip of triangle
+
+	glEnd();
+
+	
+	glFlush();
+}
+
+void resize(int width,int height)
+{
+	//code
+	if(height==0)
+		height=1;
+	glViewport(0,0,(GLsizei)width,(GLsizei)height);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	perspectiveGL(45.0f,(GLfloat)width/(GLfloat)height,0.1f,100.0f);
+
+	
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+void perspectiveGL(GLdouble fovY, GLdouble aspect, GLdouble zNear, GLdouble zFar)
+{
+     const GLdouble pi = 3.1415926535897932384626433832795;
+     GLdouble fW, fH;
+ 
+     //fH = tan( (fovY / 2) / 180 * pi ) * zNear;
+ 
+     fH = tan(fovY / 360 * pi) * zNear;
+ 
+     fW = fH * aspect;
+ 
+ 
+ 
+     glFrustum(-fW, fW, -fH, fH, zNear, zFar);
+ 
+}
+
+void uninitialize(void)
+{
+	GLXContext currentGLXContext;
+	currentGLXContext=glXGetCurrentContext();
+
+	if(currentGLXContext!=NULL && currentGLXContext==gGLXContext)
+	{
+		glXMakeCurrent(gpDisplay,0,0);
+	}
+	
+	if(gGLXContext)
+	{
+		glXDestroyContext(gpDisplay,gGLXContext);
+	}
+	
+	if(gWindow)
+	{
+		XDestroyWindow(gpDisplay,gWindow);
+	}
+	
+	if(gColormap)
+	{
+		XFreeColormap(gpDisplay,gColormap);
+	}
+	
+	if(gpXVisualInfo)
+	{
+		free(gpXVisualInfo);
+		gpXVisualInfo=NULL;	
+	}
+
+	if(gpDisplay)
+	{
+		XCloseDisplay(gpDisplay);
+		gpDisplay=NULL;
+	}
+}
+
+
